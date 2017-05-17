43d746cf359c0d887a1508f0c90c38c1644f7a24
StefanYohansson <sy.fen0@gmail.com>
2017-05-17 08:46:37 -0300

    Added read from input to provide api key.

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
 use toml;
 
 #[derive(Deserialize)]
@@ -41,7 +42,16 @@ pub fn get_config(folder: PathBuf) -> Config {
 }
 
 fn create_config(folder: PathBuf) {
+    let mut input = String::new();
+
+    println!("Provide your api key: ");
+    io::stdin().read_line(&mut input)
+        .ok()
+        .expect("Couldn't read line");
+    
     let mut file = File::create(folder).unwrap();
-    file.write_all(b"cloudshark_api = 'your-api-here'");
+    file.write_all(
+        &format!("cloudshark_api = '{}'", input.trim()).as_bytes()
+    );
     file.sync_all();
 }
